8d7d86a180f52d245303c6ef0700ba1b5ce1c143
Govinda Mahajan <govindam@pune.smartek21.st21>
2019-02-07 14:26:21 +0530

    adding about & locations routes

--- app_server/controllers/main.js
+++
@@ -1,5 +0,0 @@
-/** GET Home Page */
-module.exports.index = function (req, res) {
-    console.log(` ****** Inside homePageController ******`)
-    res.render('index', { title: 'Loc8R Express App' });
-}
--- app_server/routes/index.js
+++ app_server/routes/index.js
@@ -1,9 +1,16 @@
 'use strict'
 var express = require('express');
 var router = express.Router();
-var homePageController = require('../controllers/main');
 
-/* GET home page. */
-router.get('/', homePageController.index);
+const ctrLocations = require('../controllers/locations'),
+    ctrOthers = require('../controllers/others');
+
+/** Locations Pages */
+router.get('/', ctrLocations.homeList);
+router.get('/location', ctrLocations.locationInfo);
+router.get('/location/review/new', ctrLocations.addReview);
+
+/** Other Pages */
+router.get('/about', ctrOthers.about);
 
 module.exports = router;
